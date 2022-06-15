d4e28dd42c7bcc252ba69121b2f395927b1c7e59
Ishank-T <ishanktambi13@gmail.com>
2022-06-16 01:32:30 +0530

    Created all the postApi routes

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -282,7 +282,9 @@ router.delete('/education/:edu_id',auth, async(req,res)=>{
 router.get('/github/:username',async(req,res)=>{
     try {
         const options= {
-            uri : `https://api.github.com/users/${req.params.username}/repos?per_page=5&sort=created:asc&client_id=${config.get('githubClient')}&client_secret=${config.get('githubSecret')}`,
+            uri : `https://api.github.com/users/${req.params.username}
+            /repos?per_page=5&sort=created:asc&client_id=${config.get
+                ('githubClient')}&client_secret=${config.get('githubSecret')}`,
             method : 'GET',
             headers : {'user-agent': 'node-js'}
         };
